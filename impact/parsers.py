dae8fb3118f52820cc41872dff49bb7dcea96a15
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-09-25 11:23:27 -0700

    MAINT: unit -> pmd_unit

--- impact/parsers.py
+++ impact/parsers.py
@@ -5,7 +5,7 @@ import warnings
 import numpy as np
 import polars as pl
 from beamphysics.species import mass_of
-from beamphysics.units import multiply_units, unit
+from beamphysics.units import multiply_units, pmd_unit
 
 from . import fieldmaps, tools
 from .particles import identify_species
@@ -2382,11 +2382,11 @@ def _replace_bare_gammabeta_with_p(key, mc2):
         comp = key[10:]
         assert comp in ("x", "y", "z")
         newkey = f"p{comp}"
-        extraunits = unit("eV/c")
+        extraunits = pmd_unit("eV/c")
     else:
         factor = 1
         newkey = key
-        extraunits = unit("1")
+        extraunits = pmd_unit("1")
 
     return newkey, factor, extraunits
 
@@ -2460,7 +2460,7 @@ def load_stats(path, species="electron", types=FORT_STAT_TYPES, verbose=False):
             unit_string = "eV"
             k = newkey
 
-        u = unit(unit_string)
+        u = pmd_unit(unit_string)
 
         # Replace all gammabeta_{k} including cov_{k1}__{k2}
         newkey, factor, extraunits = _replace_all_gammabeta_with_p(k, mc2)
@@ -2512,6 +2512,6 @@ def load_slice_info(path, verbose=False):
     data1 = data[list(data)[0]]
     for k in data1:
         unit_string = UNITS[k]
-        units[k] = unit(unit_string)
+        units[k] = pmd_unit(unit_string)
 
     return data, units
